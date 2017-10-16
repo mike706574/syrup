c4d00c15eca040107d09b68cad12be756cb44fdd
EAS7510 <michaeleaster@northwesternmutual.com>
2017-10-16 14:24:57 -0500

    Release to clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/syrup-alpha "0.0.2"
+(defproject fun.mike/syrup-alpha "0.0.2-SNAPSHOT"
   :description "Flat file parsing and validation library."
   :url "https://github.com/mike706574/syrup"
   :license {:name "Eclipse Public License"
@@ -11,4 +11,6 @@
                    :target-path "target/dev"
                    :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                                   [org.clojure/tools.namespace "0.2.11"]]}}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
   :repl-options {:init-ns user})
