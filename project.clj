b704f7a26812711be00fe5dbb194fabed510f57e
EAS7510 <michaeleaster@northwesternmutual.com>
2017-11-01 23:05:29 -0500

    Version 0.0.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/syrup-alpha "0.0.12-SNAPSHOT"
+(defproject fun.mike/syrup-alpha "0.0.12"
   :description "Flat file parsing and validation library."
   :url "https://github.com/mike706574/syrup"
   :license {:name "Eclipse Public License"
